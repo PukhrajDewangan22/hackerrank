5a09cd4555fb62cf790f4d20a103f1f38f4f7f98
pukhraj Dewangan <141709835+PukhrajDewangan22@users.noreply.github.com>
2024-11-06 15:31:59 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,118 @@
+
+#hhhhjh
+#kkk#kkk#k
+"""fghfhfhfhfhfyryryfyrytre
+print("hello world" "hello",end="\n")
+"""
+"""a=5
+x=52124125.558
+print ("hello",end="\t")
+print("a=",a,sep="525")
+print(x)
+print(type(x))
+x=3
+y=float(3)
+z=int(3)
+x="atul"
+print(type(x))
+print(type(y))
+print(type(z))
+print(x)
+print(y)
+print(z)
+x,y,z="orange","mango|","papaya"
+print(x,y,z,sep=" \t ",end="\n")
+x=y=z="ornge"
+print(x,y,z,sep=" \t ",end="\n")
+print(x+y+z)
+a="string valuer"
+b="4"
+print(a+b)"""
+
+# t=("hello","my ","name","is ")
+# for s in t:
+#     print(s)
+# print(t[2])
+
+# a= {0:"Chaitanya",1:"Singh",2:"Chaitanya", "age":31}
+# print(a["age"])
+# Set Example 
+# myset = {"hi", 2, "bye", "Hello World"} 
+
+# for a in myset: 
+#     print(a) 
+# print(2 in myset)
+# myset.add(99) 
+# print(myset)
+# x=10
+# y=2
+# a=str(x)
+# print(type(a))
+# print(bool("hello"))
+# print(bool(0))
+# # print(x-y)
+
+# # a=10
+# # if(a<10):
+# #     print("hello")
+# # elif(a==10):
+# #     print("equal")
+
+# # else:
+# #     print("false")
+
+# # print(tuple(range(10,50,5)))
+
+# i=1
+# while i<6:
+#     print(i)
+#     if i== 4:
+#       break
+#     i+=1
+
+# for x in i:
+#    print(x)
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(i,end="\t")
+#     print()
+# a= "pukhraj dewangan"
+
+# print(a)
+
+# print(a[:5:-1])
+
+# thislist = ["banana","Orange","Kiwi","cherry"]
+# thislist.reverse()  
+# print(thislist)      
+# def my_function(fname, lname):
+
+#   print(fname + " " + lname)
+# my_function("Emil", "Refsnes")
+# def my_function(x): 
+#   return 5* x
+
+# my_function("india")
+# print(my_function(3))
+# x= lambda a:a+10
+# print(x(25))
+# class Person:
+#     def _init_(self, name, age):
+#       self.name = age
+#       self.age = name
+
+
+# p1 = Person("John", 36)
+# print(p1.name)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = age
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+del p1
+print(p1.age)
